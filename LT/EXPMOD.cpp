8c73fc171dab3eb90c59e42d1223055d6316822d
Anonymous <103519498+twilight2368@users.noreply.github.com>
2023-12-22 15:04:02 +0700

    now

--- LT/EXPMOD.cpp
+++ LT/EXPMOD.cpp
@@ -5,28 +5,40 @@
 
 using namespace std;
 
-int modularNum(){
-    long long a = 10;
-    for (int i = 1; i < 10; i++)
-    {
-        a = a*a;
-    }
-    
-    return a + 7;
+long modularNum(){
+    return 1e9 + 7;
 }
 
-int z = modularNum();
-
-int exponentMod(long long x, long long y)
+int exponentMod(long long x, long long y, long z)
 {
+    long num = 0;
+    long a = 0;
+
+    if (x == 0){
+        return 0;
+    }
+    if (y == 0)
+    {
+        return 1;
+    }
+
+    if (y % 2 == 0)
+    {
+        a = exponentMod(x, y/2, z);
+        num = (a*a) % z;
+    }else{
+        a = exponentMod(x, y - 1, z);
+        num = (x % z * a) % z;
+    }
     
+    return (int) (num + z) % z;
 }
 
 int main(){
     long long x, y;
     cin >> x >> y;
-
-    cout << exponentMod(x, y);
-    
+    long z = modularNum();
+    //x^y % (10^9 -7)
+    cout << exponentMod(x, y, z) << endl;
 
 }
---
+++ TN/GoldMining.md
@@ -0,0 +1,19 @@
+# Description
+The Kingdom ALPHA has n warehouses of golds located on a straight line and are numbered 1, 2,..., n. The warehouse i has amount of a
+i (aiai is non-negative integer) and is located at coordinate i (i=1,...,n). The King of ALPHA opens a competition for hunters who are responsible to find a subset of gold warehouses having largest total amount of golds with respect to the condition that the distance between two selected warehouses must be greater than or equal to L1 and less than or equal to L2.
+
+# Input
+Line 1 contains n, L1, and L2 (1≤n≤1000000,1≤L1≤L2≤n)  
+Line 2 contains n integers a1,a2,…,an
+# Output
+Contains only one single integer denoting the total amount of golds of selected warehouses.
+# Example
+## Input
+```
+6 2 3 
+3 5 9 6 7 4 
+```
+## Output
+```
+19
+```
